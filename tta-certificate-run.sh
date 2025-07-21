0601a0e5b939504182eebfcaf971fe8b5f4174dc
travis1829 <travis1829@naver.com>
2025-07-21 07:37:40 +0000

    Small fix in tta script

--- tta-certificate-run.sh
+++ tta-certificate-run.sh
@@ -1,5 +1,5 @@
 KERNEL_VERSION=$(uname -r)
-if [ "$KERNEL_VERSION" != "6.8.0" ] && [ "$KERNEL_VERSION" != "6.8.0-interval-vm-debug+" ]; then
+if [ "$KERNEL_VERSION" != "6.8.0-debug" ] && [ "$KERNEL_VERSION" != "6.8.0-interval-vm-debug+" ]; then
     echo "Error: Kernel version is not 6.8.0 or 6.8.0-interval-vm-debug+ (found $KERNEL_VERSION)"
     exit 1
 fi
